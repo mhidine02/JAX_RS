0825bfdde02ec9045bfad59d02efb67ec87fbb31
mhdine02 <younes.moh.fst@uhp.ac.ma>
2024-11-17 21:18:30 +0100

    first commit

---
+++ src/main/java/ma/rest/spring/entities/Compte.java
@@ -0,0 +1,27 @@
+package ma.rest.spring.entities;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JacksonXmlRootElement(localName = "compte")
+public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double solde;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+
+    @Enumerated(EnumType.STRING)
+    private TypeCompte type;
+}
